892769473bb163a284bb1df6124e48ec0486605f
r.harkes_laptop <r.harkes@nki.nl>
2022-11-29 17:11:15 +0100

    remove self

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath(".."))
-
-import pyhaloxml
 
 # -- Project information
 project = 'pyhaloxml'
