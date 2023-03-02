1097afe93695b0bfc5b39f7ff9a2b268fc93b255
r.harkes_laptop <r.harkes@nki.nl>
2023-03-02 15:10:38 +0100

    error finding the pyx file in workflow

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import setuptools
 import Cython.Build
+from pathlib import Path
 
 setuptools.setup(
     name="c_inpoly",
     ext_modules=Cython.Build.cythonize(
-        r"src\pyhaloxml\cython\c_inpoly.pyx",
+        str(Path('src', 'pyhaloxml', 'cython', 'c_inpoly.pyx')),
         compiler_directives={"language_level": "3"},
     ),
     ext_package="pyhaloxml.cython",
