a9de7ec5ea286e980c849e87c73b61386489b7b6
r.harkes_laptop <r.harkes@nki.nl>
2023-03-06 15:31:19 +0100

    mypy needs one python version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
-import Cython.Build
+from Cython.Build import cythonize
 from pathlib import Path
 
 setuptools.setup(
     name="c_inpoly",
-    ext_modules=Cython.Build.cythonize(
-        str(Path("src", "pyhaloxml", "cython", "c_inpoly.pyx")),
+    ext_modules=cythonize(
+        [str(Path("src", "pyhaloxml", "cython", "c_inpoly.pyx"))],
         compiler_directives={"language_level": "3"},
     ),
     ext_package="pyhaloxml.cython",
