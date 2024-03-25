c7c1a75aee2af352f50120eb559cee0f64fc1fe6
r.harkes <r.harkes@nki.nl>
2024-03-25 16:05:02 +0100

    explicitly export

--- src/pyhaloxml/__init__.py
+++ src/pyhaloxml/__init__.py
@@ -3,3 +3,5 @@ from .Region import Region
 from .Layer import Layer
 from .misc import RegionType
 from .version import __version__
+
+__all__ = ["HaloXML", "HaloXMLFile", "Region", "Layer", "RegionType", "__version__"]
--- src/pyhaloxml/shapely/__init__.py
+++ src/pyhaloxml/shapely/__init__.py
@@ -1,1 +1,3 @@
 from .to_shapely import layer_to_shapely, region_to_shapely
+
+__all__ = ["layer_to_shapely", "region_to_shapely"]
