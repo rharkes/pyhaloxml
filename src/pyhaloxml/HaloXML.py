e95fcf85265c0dcef9be8e081461673ea21e114f
rharkes <rolf@harkes.nu>
2024-10-07 23:19:41 +0200

    numpydoc

--- src/pyhaloxml/HaloXML.py
+++ src/pyhaloxml/HaloXML.py
@@ -5,11 +5,18 @@ It can load .annotation files and store them as .geojson. These must be QuPath c
 
 Examples
 --------
+Converting an annotations file to geojson:
 >>> from pyhaloxml import HaloXML
 >>> hx = HaloXML()
 >>> hx.load(r'c:\test.annotations')
 >>> hx.matchnegative()
 >>> hx.to_geojson(r'c:\test.geojson')
+
+Using the contextmanager to do the same thing:
+>>> from pyhaloxml import HaloXMLFile
+>>> with HaloXML(r'c:\test.annotations') as hx:
+>>>     hx.matchnegative()
+>>>     hx.to_geojson(r'c:\test.geojson')
 """
 
 import io
